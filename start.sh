e4e161c46a0414872dd8c6e1e7094b9befc90c39
David Siaw <davidsiaw@gmail.com>
2017-06-13 00:25:55 +0900

    fix bad proxying and websocket proxying

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-envsubst < /conf.template > /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'
+DOLLAR=$ envsubst < /conf.template > /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'
